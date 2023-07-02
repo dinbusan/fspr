718f96881b1b0c914e2bc7221cb096f1ea224217
dinbusan <deann.videtto@gmail.com>
2023-07-02 12:33:04 +0200

    centered logo on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,9 @@ import Logo from "/assets/logo_no_bg.png";
 
 const Home = () => {
   return (
-    <div className="flex justify-center align-center bg-gradient-to-br from-neutral-950 to-pink-600 ">
-      <div>
-        <img
-          className="mix-blend-darken w-1/2 object-cover"
-          src={Logo}
-          alt=""
-        />
+    <div className="flex h-[100vh] relative bg-gradient-to-br from-neutral-950 to-pink-600 ">
+      <div className="mix-blend-darken w-full flex justify-center">
+        <img className="object-cover h-3/4" src={Logo} alt="" />
       </div>
     </div>
   );
