ee64c233f48ae521b269eea8eb036bf5b86006e8
dinbusan <deann.videtto@gmail.com>
2023-07-04 17:56:20 +0200

    added a smoky bg and took away logo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,23 +3,20 @@ import Logo from "/assets/logo_white_no_bg_edited.png";
 
 const Home = () => {
   return (
-    <div className="pt-16">
-      <div className="md:flex flex-row justify-center">
-        <h1 className="hidden md:block font-antique text-transparent text-7xl text-right w-1/2 ms-3 bg-gradient-to-r from-black to-zinc-400 bg-clip-text">
-          Forsaken Souls Paranormal Research
-        </h1>
-        {/* <h1 className="flex md:hidden font-antique text-white text-5xl ms-10 text-left">
-          Forsaken Souls Paranormal Research
-        </h1> */}
-        {/* <h1 className="block md:hidden font-antique mx-auto text-white text-lg ms-10 text-left">
-          Saving the forsaken, speaking for the silent.
-        </h1> */}
-        <div className="w-full mx-auto md:me-10 relative bg-gradient-to-r from-black to-zinc-100 flex items-center">
-          <img
-            className="object-cover	absolute mix-blend-overlay"
-            src={Logo}
-            alt=""
-          />
+    <div className="">
+      <div className="bg-gradient-to-br from-zinc-800 to-zinc-400 h-96 w-full relative text-right pe-10">
+        <img
+          className="w-full h-full object-cover absolute mix-blend-hard-light"
+          src="/assets/smoke_bg.jpg"
+          alt=""
+        />
+
+        {/* <div className="absolute inset-x-0 bottom-0 h-24 "></div> */}
+
+        <div className="pt-10">
+          <h1 className="font-antique text-7xl text-white">
+            Forsaken Souls Paranormal Research
+          </h1>
         </div>
       </div>
     </div>
