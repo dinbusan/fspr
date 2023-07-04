63e417a42a1359e6b763e76b715ddc7b963a5663
dinbusan <deann.videtto@gmail.com>
2023-07-04 12:47:49 +0200

    updated bg-gradient home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,9 @@ import Logo from "/assets/logo_white_no_bg_edited.png";
 
 const Home = () => {
   return (
-    <div className="pt-10">
-      <div className="md:flex flex-row justify-center items-center">
-        <h1 className="hidden md:block font-antique text-transparent text-7xl text-right w-full ms-3 bg-gradient-to-r from-black to-zinc-400 bg-clip-text">
+    <div className="pt-16">
+      <div className="md:flex flex-row justify-center">
+        <h1 className="hidden md:block font-antique text-transparent text-7xl text-right w-1/2 ms-3 bg-gradient-to-r from-black to-zinc-400 bg-clip-text">
           Forsaken Souls Paranormal Research
         </h1>
         {/* <h1 className="flex md:hidden font-antique text-white text-5xl ms-10 text-left">
@@ -14,9 +14,9 @@ const Home = () => {
         {/* <h1 className="block md:hidden font-antique mx-auto text-white text-lg ms-10 text-left">
           Saving the forsaken, speaking for the silent.
         </h1> */}
-        <div className="w-full mx-auto md:me-10 -mt-10 md:mt-0">
+        <div className="w-full mx-auto md:me-10 relative bg-gradient-to-r from-black to-zinc-100 flex items-center">
           <img
-            className="bg-blend-darken brightness-200 md:brightness-90 object-cover	"
+            className="object-cover	absolute mix-blend-overlay"
             src={Logo}
             alt=""
           />
